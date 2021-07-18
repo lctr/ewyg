1cefec6645f1a4566615b41614d1f0faee1dc7b7
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-18 06:18:03 -0500

    removed commented server deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,4 @@
-// import { serve } from "https://deno.land/std@0.99.0/http/server.ts";
-// export { Application, Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 
-// export { oakCors } from "https://deno.land/x/cors/mod.ts";
-
+// TODO: WYG BUNDLE VERSIONS
 export { Wyg } from "./lib/wygweb.bundle.js";
 

b3c60683cafaf94ccda797dab47a41764b659557
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-18 06:19:14 -0500

    boilerplate

---
+++ src/components/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Editor from "@/components/Editor.svelte";
+  export let name;
+</script>
+
+<h1>Wyg</h1>
+<p>Ah</p>
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:100,400"
+  rel="stylesheet"
+/>
+<Editor />
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>

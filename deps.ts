81c2fc86c470ae92a7301e45ce9eecd3aa7f1f5f
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-18 06:16:08 -0500

    +wyg deps

---
+++ deps.ts
@@ -0,0 +1,7 @@
+// import { serve } from "https://deno.land/std@0.99.0/http/server.ts";
+// export { Application, Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+
+// export { oakCors } from "https://deno.land/x/cors/mod.ts";
+
+export { Wyg } from "./lib/wygweb.bundle.js";
+

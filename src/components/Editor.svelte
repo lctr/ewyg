39ba4a98b9e80d61408c471a651a33fec37817c5
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-19 18:43:14 -0500

    moved results to same pane; pane colors dynamic to failure or success; removed html markup spaces in input

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,59 +1,108 @@
 <script>
 import Pane from './layout/Pane.svelte';
-import * as Wyg from "../../lib/wygweb.bundle";
+import { Wyg } from "../../lib/wygweb.bundle";
 
-const Egg = new Wyg.Wyg();
-const EMPTY = '~ ~ ~';
-let INPUT = 'print("hello!")';
-let AST = EMPTY;
+const Egg = new Wyg();
+const EMPTY = '· · ·';
+let PLACEHOLDER = "Enter some Wyg code!";
+let INPUT;
+let AST = '';
 let RESULT = EMPTY;
 let MESSAGE = EMPTY;
+let ERROR;
+let session; 
+const editor = {title: "Editor", color: "blue"};
+const syntax = {title: "Tree", color: "green"};
 
-function handleInput(event) {
-  if (!INPUT.trim().endsWith(';')) return;
+function handleInput(e) {
   try {
-    let session = Egg.run(INPUT);
+    session = Egg.run(INPUT.replaceAll('&nbsp;', ''));
     if (session.error) {
-      MESSAGE = session.message;
+      if (Egg.parseError) {
+        AST = Egg.parseError;
+        MESSAGE = 'Error parsing input!';
+      } else {
+        MESSAGE = session.message;
+      }
+      syntax.color = "red";
+      editor.color = "purple";
+      ERROR = true;
+      Egg.reset();
     } else {
       AST = JSON.stringify(session.ast, null, 2);
+      session.error = void 0;
       RESULT = session.value;
+      syntax.color = "green";
+      editor.color = "blue";
+      ERROR = false;
     }
   } catch (e) {
     console.log(e);
     MESSAGE = session.message;
+    ERROR = true;
+    Egg.reset();
   }
 }
 
-const editor = {title: "Editor", color: "blue"};
-const result = {title: "Result", color: "red"};
-const syntax = {title: "Tree", color: "green"};
+
 </script>
 
 <div class="active-panes">
   <Pane {...editor}>
-    <div id="editing" contenteditable="true"
-        on:keyup="{handleInput}" 
-        bind:innerHTML={INPUT} />
-  </Pane>
+    <div id="editing" 
+      contenteditable="true" 
+      data-placeholder={PLACEHOLDER}
+      on:keyup="{handleInput}" 
+      bind:innerHTML={INPUT} />
 
-  <Pane {...result}>{RESULT}</Pane>
+    <div id="result">
+      {#if ERROR }
+        <pre>&gt; {MESSAGE}</pre>
+      {:else}
+        <code>&gt; {RESULT}</code>
+      {/if}
+    </div>
+  </Pane>
     
-  <Pane {...syntax}><pre>{AST}</pre></Pane>
+  <Pane {...syntax}>
+    <pre>{AST}</pre>
+  </Pane>
 </div>
 
 <style>
+
+.active-panes {
+	margin-top: 0.75em;
+	display: grid;
+	flex: 2 1 auto;
+	flex-direction: column;
+	gap: 0.75em;
+	align-items: start;
+}
+
+#editing:empty:not(:focus):before {
+  content: attr(data-placeholder);
+  color: gray;
+}
 
 #editing {
   font-size: 13pt;
   font-family: monospace;
   line-height: 20pt;
   width: 100%;
+  height: 250px;
   z-index: 1;
   outline: none;
   background: transparent;
   caret-color: red;
+  /* line-height: 1.1; */
   text-align: left;
+}
+
+#result {
+  width: 100%; 
+  padding: 2px 0;
+  border-top: 1px solid #999;
 }
 
 .error {

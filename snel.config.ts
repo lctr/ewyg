1686dc0055b13ff8052e348f9ce45afdc54deb62
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-19 20:08:18 -0500

    deno -> node

--- snel.config.ts
+++
@@ -1,9 +0,0 @@
-import type { snelConfig } from "https://deno.land/x/snel/mod.ts";
-
-export default <snelConfig> {
-  root: "src/components/App.svelte",
-  port: 3000,
-  mode: "dom",
-  plugins: [],
-  extendsImportMap: [],
-};

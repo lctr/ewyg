f1d54067e606aab72c81f2698306660fe7fac16d
Lictor Guzman <lctrgzmn@gmail.com>
2021-07-22 18:49:20 -0500

    nav/menu frame

---
+++ src/components/layout/Topbar.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<div id="top-bar">
+  <div>
+    <slot></slot>
+  </div>
+</div>
+
+<style>
+#top-bar {
+	top: 0;
+	left: 0;
+	z-index: 999;
+	width: 100vw;
+	height: 3em;
+	display: flex;
+	position: fixed;
+	column-gap: 0.75em;
+	align-items: center;
+	box-shadow: 0 0 5px #666;
+	backdrop-filter: blur(2px);
+	background-image: linear-gradient(rgba(67, 67, 67, 1), 10%, rgba(67, 67, 67, 0.85));
+}
+
+#top-bar div {
+	color: #fff;
+	display: grid;
+	padding: 4px 4px;
+	column-gap: 0.75em;
+	grid-auto-flow: column;
+}
+</style>
